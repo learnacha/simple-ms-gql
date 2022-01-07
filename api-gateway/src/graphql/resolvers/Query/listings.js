9372fd608abcd00ffb792c113b05b8229a75118c
Sri Hari Acha <learnacha@gmail.com>
2022-01-07 22:18:59 +0400

    added api gateway for ms

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,11 @@
+const listingsResolver = async () => {
+  return [
+    {
+      id: 1,
+      title: "test",
+      description: "sri",
+    },
+  ];
+};
+
+export default listingsResolver;
---
+++ api-gateway/src/server/startServer.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "apollo-server-express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+
+import resolvers from "#root/graphql/resolvers";
+import typeDefs from "#root/graphql/typeDefs";
+import accessEnv from "#root/helpers/accessEnv";
+
+const PORT = accessEnv("PORT", 7000);
+
+const apolloServer = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: (origin, cb) => cb(null, true),
+    credentials: true,
+  })
+);
+
+(async () => {
+  await apolloServer.start();
+  await apolloServer.applyMiddleware({ app, cors: false, path: "/graphql" });
+})();
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`api gateway is listening at ${PORT}`);
+});
